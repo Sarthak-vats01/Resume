82e4b831b035b18e9c93c0970d2100eece440ebd
Sarthak Vats <sarthakvats121212@gmail.com>
2024-09-28 17:29:44 +0530

    second

--- portfolio/src/component/Title.jsx
+++ portfolio/src/component/Title.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import myImage from "../assets/My-Photo.jpg";
 import { useNavigate } from "react-router-dom";
+import resume from "../assets/Resume.pdf"; // Correctly import the resume PDF
 
 function Title() {
   const navigate = useNavigate();
+
   return (
-    <div className="flex justify-center items-center flex-grow  p-8 ">
+    <div className="flex justify-center items-center flex-grow p-8">
       <img
         className="mr-8 rounded-full w-96 h-96 object-cover filter grayscale"
         src={myImage}
@@ -14,7 +16,7 @@ function Title() {
       <div className="flex flex-start flex-col">
         <h1 className="text-8xl font-bold">Hello</h1>
         <h3 className="text-xl font-semibold mt-8 ml-2">A Bit About Me</h3>
-        <p className="mt-2 ml-2 leading-7 font-extralight  text-gray-800">
+        <p className="mt-2 ml-2 leading-7 font-extralight text-gray-800">
           I'm a full-stack developer proficient in MERN Stack, .NET, and <br />
           Angular, with a passion for building robust applications. I specialize
           <br />
@@ -25,18 +27,27 @@ function Title() {
         </p>
 
         <div className="flex justify-between w-10/12 font-semibold mt-4">
-          <div className="flex rounded-full w-32 h-32 border border-black justify-center items-center bg-yellow-500 hover:bg-inherit transition-colors duration-150 ease-in-out">
+          {/* Resume download button */}
+          <a
+            className="flex rounded-full w-32 h-32 border border-black justify-center items-center bg-yellow-500 hover:bg-inherit transition-colors duration-150 ease-in-out"
+            href={resume} // Correctly linking to the resume file
+            download="Sarthak_Vats_Resume.pdf"
+          >
             Resume
-          </div>
-          <div
+          </a>
+
+          {/* Navigate to Projects */}
+          <button
             className="flex rounded-full w-32 h-32 border border-black justify-center items-center bg-red-600 hover:bg-inherit transition-colors duration-150 ease-in-out"
             onClick={() => navigate("/projects")}
           >
             Projects
-          </div>
-          <div className="flex rounded-full w-32 h-32 border border-black justify-center items-center bg-blue-300 hover:bg-inherit transition-colors duration-150 ease-in-out">
+          </button>
+
+          {/* Contact button (could be improved by linking to a contact section) */}
+          <button className="flex rounded-full w-32 h-32 border border-black justify-center items-center bg-blue-300 hover:bg-inherit transition-colors duration-150 ease-in-out">
             Contact
-          </div>
+          </button>
         </div>
       </div>
     </div>
